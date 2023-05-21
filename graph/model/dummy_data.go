b41dbe1f7bb6dde3032b8f734ef09ae5997001fe
Muhammad Saleh Solahudin <alexcomunnity242@gmail.com>
2023-05-21 12:48:10 +0700

    update dummy data

--- graph/model/dummy_data.go
+++ graph/model/dummy_data.go
@@ -4,19 +4,19 @@ var DummyMeetups = []*Meetup{
 	{
 		ID:          "1",
 		Name:        "A first meetup for Bob",
-		Description: "A description",
+		Description: "A description first meetup for Bob",
 		UserID:      "1",
 	},
 	{
 		ID:          "2",
 		Name:        "A first meetup for Jon",
-		Description: "A description",
+		Description: "A description first meetup for Jon",
 		UserID:      "2",
 	},
 	{
 		ID:          "3",
 		Name:        "A second meetup for Jon",
-		Description: "A description",
+		Description: "A description second meetup for Jon",
 		UserID:      "2",
 	},
 }
@@ -31,5 +31,15 @@ var DummyUsers = []*User{
 		ID:       "2",
 		Username: "Jon",
 		Email:    "jon@gmail.com",
+	},
+	{
+		ID:       "3",
+		Username: "Jane",
+		Email:    "jane@gmail.com",
+	},
+	{
+		ID:       "4",
+		Username: "John",
+		Email:    "john@gmail.com",
 	},
 }
